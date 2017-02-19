db2b9fe997b9168250a41e372970f5d40db171bc
Henrik Schinzel <henrik@schinzel.io>
2017-02-19 19:46:11 +0100

    Test added

--- src/test/java/io/schinzel/basicutils/state/StateTest.java
+++ src/test/java/io/schinzel/basicutils/state/StateTest.java
@@ -17,26 +17,40 @@ public class StateTest {
 
     @Test
     public void testSomeMethod() {
-        Map<String, Object> props = State.create()
+        State state = State.getBuilder()
                 .add("string", "a string")
-                .add("int", 777)
-                .add("large int", 123456789)
-                .add("long", 1234567890l)
-                .add("double not rounded", 0.3333d)
-                .add("double rounded", 0.4444d, 3)
-                .add("large double rounded", 123456.4444d, 2)
-                .add("float not rounded", 0.5555f)
-                .add("float rounded", 0.6666f, 3)
-                .getProperties();
-        Assert.assertEquals("a string", props.get("string"));
-        Assert.assertEquals("777", props.get("int"));
-        Assert.assertEquals("123 456 789", props.get("large int"));
-        Assert.assertEquals("1 234 567 890", props.get("long"));
-        Assert.assertEquals("0.33", props.get("double not rounded"));
-        Assert.assertEquals("0.444", props.get("double rounded"));
-        Assert.assertEquals("0.56", props.get("float not rounded"));
-        Assert.assertEquals("0.667", props.get("float rounded"));
-    }
+                .add("the_int", 123456789)
+                .add("the_long", 1234567890l)
+                .add("the_double", 0.4444d, 2)
+                .add("the_float", 12356.6666f, 3)
+                .build();
+        List<Property> props = state.mProperties;
+        //Check string
+        Assert.assertEquals("string", props.get(0).mKey);
+        Assert.assertEquals("a string", props.get(0).mValueAsString);
+        Assert.assertEquals("string:a string", props.get(0).getString());
+        Assert.assertEquals("a string", props.get(0).getObject());
+        //Check int        
+        Assert.assertEquals("the_int", props.get(1).mKey);
+        Assert.assertEquals("123456789", props.get(1).mValueAsString);
+        Assert.assertEquals("the_int:123456789", props.get(1).getString());
+        Assert.assertEquals(new Long(123456789), props.get(1).getObject());
+        //Check long
+        Assert.assertEquals("the_long", props.get(2).mKey);
+        Assert.assertEquals("1234567890", props.get(2).mValueAsString);
+        Assert.assertEquals("the_long:1234567890", props.get(2).getString());
+        Assert.assertEquals(new Long(1234567890), props.get(2).getObject());
+        //Check double
+        Assert.assertEquals("the_double", props.get(3).mKey);
+        Assert.assertEquals("0.44", props.get(3).mValueAsString);
+        Assert.assertEquals("the_double:0.44", props.get(3).getString());
+        Assert.assertEquals(new Double(0.4444d), props.get(3).getObject());
+        //Check float
+        Assert.assertEquals("the_float", props.get(4).mKey);
+        Assert.assertEquals("12 356.667", props.get(4).mValueAsString);
+        Assert.assertEquals("the_float:12 356.667", props.get(4).getString());
+        Assert.assertEquals(new Double(12356.6666f), props.get(4).getObject());
+ }
 
 
     @Test
