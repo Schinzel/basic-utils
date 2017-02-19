0a65cabf52b9a20e8d22d2cebfa155e1871339ee
Henrik Schinzel <henrik@schinzel.io>
2017-02-19 20:22:31 +0100

    Broke up long test method

--- src/test/java/io/schinzel/basicutils/state/StateTest.java
+++ src/test/java/io/schinzel/basicutils/state/StateTest.java
@@ -16,41 +16,63 @@ import org.junit.Test;
 public class StateTest {
 
     @Test
-    public void testSomeMethod() {
-        State state = State.getBuilder()
+    public void testStringProperty() {
+        List<Property> props = State.getBuilder()
                 .add("string", "a string")
-                .add("the_int", 123456789)
-                .add("the_long", 1234567890l)
-                .add("the_double", 0.4444d, 2)
-                .add("the_float", 12356.6666f, 3)
-                .build();
-        List<Property> props = state.mProperties;
-        //Check string
+                .build().mProperties;
         Assert.assertEquals("string", props.get(0).mKey);
         Assert.assertEquals("a string", props.get(0).mValueAsString);
         Assert.assertEquals("string:a string", props.get(0).getString());
         Assert.assertEquals("a string", props.get(0).getObject());
-        //Check int        
-        Assert.assertEquals("the_int", props.get(1).mKey);
-        Assert.assertEquals("123 456 789", props.get(1).mValueAsString);
-        Assert.assertEquals("the_int:123 456 789", props.get(1).getString());
-        Assert.assertEquals(new Long(123456789), props.get(1).getObject());
-        //Check long
-        Assert.assertEquals("the_long", props.get(2).mKey);
-        Assert.assertEquals("1 234 567 890", props.get(2).mValueAsString);
-        Assert.assertEquals("the_long:1 234 567 890", props.get(2).getString());
-        Assert.assertEquals(new Long(1234567890), props.get(2).getObject());
-        //Check double
-        Assert.assertEquals("the_double", props.get(3).mKey);
-        Assert.assertEquals("0.44", props.get(3).mValueAsString);
-        Assert.assertEquals("the_double:0.44", props.get(3).getString());
-        Assert.assertEquals(new Double(0.4444d), props.get(3).getObject());
-        //Check float
-        Assert.assertEquals("the_float", props.get(4).mKey);
-        Assert.assertEquals("12 356.667", props.get(4).mValueAsString);
-        Assert.assertEquals("the_float:12 356.667", props.get(4).getString());
-        Assert.assertEquals(new Double(12356.6666f), props.get(4).getObject());
- }
+    }
+
+
+    @Test
+    public void testIntProperty() {
+        List<Property> props = State.getBuilder()
+                .add("the_int", 123456789)
+                .build().mProperties;
+        Assert.assertEquals("the_int", props.get(0).mKey);
+        Assert.assertEquals("123 456 789", props.get(0).mValueAsString);
+        Assert.assertEquals("the_int:123 456 789", props.get(0).getString());
+        Assert.assertEquals(new Long(123456789), props.get(0).getObject());
+    }
+
+
+    @Test
+    public void testLongProperty() {
+        List<Property> props = State.getBuilder()
+                .add("the_long", 1234567890l)
+                .build().mProperties;
+        Assert.assertEquals("the_long", props.get(0).mKey);
+        Assert.assertEquals("1 234 567 890", props.get(0).mValueAsString);
+        Assert.assertEquals("the_long:1 234 567 890", props.get(0).getString());
+        Assert.assertEquals(new Long(1234567890), props.get(0).getObject());
+    }
+
+
+    @Test
+    public void testDoubleProperty() {
+        List<Property> props = State.getBuilder()
+                .add("the_double", 0.4444d, 2)
+                .build().mProperties;
+        Assert.assertEquals("the_double", props.get(0).mKey);
+        Assert.assertEquals("0.44", props.get(0).mValueAsString);
+        Assert.assertEquals("the_double:0.44", props.get(0).getString());
+        Assert.assertEquals(new Double(0.4444d), props.get(0).getObject());
+    }
+
+
+    @Test
+    public void testFloagProperty() {
+        List<Property> props = State.getBuilder()
+                .add("the_float", 12356.6666f, 3)
+                .build().mProperties;
+        Assert.assertEquals("the_float", props.get(0).mKey);
+        Assert.assertEquals("12 356.667", props.get(0).mValueAsString);
+        Assert.assertEquals("the_float:12 356.667", props.get(0).getString());
+        Assert.assertEquals(new Double(12356.6666f), props.get(0).getObject());
+    }
 
 
     @Test
@@ -64,9 +86,9 @@ public class StateTest {
                 .build().mProperties;
         Long prevValue = 0l;
         for (Property prop : props) {
-            Assert.assertThat("value", (Long)prop.getObject(),
+            Assert.assertThat("value", (Long) prop.getObject(),
                     greaterThan(prevValue));
-            prevValue = (Long)prop.getObject();
+            prevValue = (Long) prop.getObject();
         }
     }
 
@@ -132,6 +154,5 @@ public class StateTest {
         String expected = "{A,B,C}";
         Assert.assertThat(result, CoreMatchers.containsString(expected));
     }
-
 
 }
