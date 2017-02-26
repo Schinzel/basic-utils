01d136dc94b6d4101a3340de77e72a0e4f7611c5
Henrik Schinzel <henrik@schinzel.io>
2017-02-26 08:30:15 +0100

    Test added

---
+++ src/test/java/io/schinzel/basicutils/collections/IdSetValueTest.java
@@ -0,0 +1,59 @@
+package io.schinzel.basicutils.collections;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+import static org.hamcrest.Matchers.greaterThan;
+
+/**
+ * Created by schinzel on 2017-02-26.
+ */
+public class IdSetValueTest {
+    class MyClass implements IdSetValue{
+        int mOrder;
+        String mId;
+
+        MyClass(int order, String id){
+            mOrder = order;
+            mId = id;
+        }
+
+        @Override
+        public String getid() {
+            return mId;
+        }
+    }
+
+    @Test
+    public void compareTo() throws Exception {
+        List<MyClass> list = Arrays.asList(
+                new MyClass(2, "Ab"),
+                new MyClass(1, "aa"),
+                new MyClass(3, "B"),
+                new MyClass(9, "tB"),
+                new MyClass(4, "C"),
+                new MyClass(5, "D"),
+                new MyClass(7, "OB"),
+                new MyClass(6, "oA"),
+                new MyClass(8, "TA"),
+                new MyClass(10, "Ö"),
+                new MyClass(0, "-")
+        );
+        list = list.stream().sorted().collect(toList());
+        String s = list.stream().map(MyClass::getid).collect(joining(","));
+        System.out.println(s);
+        int prev = -100;
+        for(MyClass myClass: list){
+            int current = myClass.mOrder;
+            Assert.assertThat(current, greaterThan(prev));
+            prev = current;
+        }
+
+    }
+
+}
