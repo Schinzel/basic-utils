06eaa33911993e9f35c094f824af486e04c0625b
Henrik Schinzel <henrik.schinzel@kollektiva.se>
2017-02-28 16:54:38 +0100

    Clearer code

--- src/main/java/io/schinzel/basicutils/IThrower.java
+++ src/main/java/io/schinzel/basicutils/IThrower.java
@@ -7,13 +7,15 @@ import io.schinzel.basicutils.str.Str;
  */
 interface IThrower {
 
-    default void throwIfTrue(boolean boo, String message, String... keyValues) {
-        String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
-        Str str = Str.create().a(message).asp()
-                .a("In class: ").aq(this.getClass().getSimpleName()).a(". ")
-                .a("Method: ").aq(methodName).a(". ");
-        str.a(Helper.getArgs(keyValues));
-        throw new RuntimeException(str.getString());
+    default void throwIfTrue(boolean expression, String message, String... keyValues) {
+        if (expression) {
+            String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+            Str str = Str.create().a(message).asp()
+                    .a("In class: ").aq(this.getClass().getSimpleName()).a(". ")
+                    .a("Method: ").aq(methodName).a(". ")
+                    .a(Helper.getArgs(keyValues));
+            throw new RuntimeException(str.getString());
+        }
     }
 
 
