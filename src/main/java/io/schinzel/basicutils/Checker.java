0495ad7eeef7ab4e8c591b29106911a5faa7aaeb
Schinzel <henrik@schinzel.se>
2017-04-13 17:50:19 +0200

    isNotEmpty methods added

--- src/main/java/io/schinzel/basicutils/Checker.java
+++ src/main/java/io/schinzel/basicutils/Checker.java
@@ -18,7 +18,6 @@ public class Checker {
 
 
     /**
-     *
      * @param value The value to check
      * @return True if argument is null, else false.
      */
@@ -28,7 +27,15 @@ public class Checker {
 
 
     /**
-     *
+     * @param value The value to check
+     * @return True if argument is not null, else false.
+     */
+    public static boolean isNotEmpty(Object value) {
+        return (value != null);
+    }
+
+
+    /**
      * @param value The value to check
      * @return True if argument is null or empty, else false.
      */
@@ -38,7 +45,15 @@ public class Checker {
 
 
     /**
-     *
+     * @param value The value to check
+     * @return True if argument is not null or empty, else false.
+     */
+    public static boolean isNotEmpty(List value) {
+        return !Checker.isEmpty(value);
+    }
+
+
+    /**
      * @param value The value to check
      * @return True if argument is null or empty, else false.
      */
@@ -48,7 +63,15 @@ public class Checker {
 
 
     /**
-     *
+     * @param value The value to check
+     * @return True if argument is not null nor empty, else false.
+     */
+    public static boolean isNotEmpty(Map value) {
+        return !Checker.isEmpty(value);
+    }
+
+
+    /**
      * @param value The value to check
      * @return True if argument is null or empty, else false.
      */
@@ -56,12 +79,17 @@ public class Checker {
         return (value == null || value.length() == 0);
     }
 
-    //*************************************************************************
-    //* Array of Primitives
-    //*************************************************************************
 
     /**
-     *
+     * @param value The value to check
+     * @return True if argument is not null nor empty, else false.
+     */
+    public static boolean isNotEmpty(String value) {
+        return !Checker.isEmpty(value);
+    }
+
+
+    /**
      * @param value The value to check
      * @return True if argument is null or empty, else false.
      */
@@ -71,7 +99,15 @@ public class Checker {
 
 
     /**
-     *
+     * @param value The value to check
+     * @return True if argument is not null nor empty, else false.
+     */
+    public static boolean isNotEmpty(byte[] value) {
+        return !Checker.isEmpty(value);
+    }
+
+
+    /**
      * @param value The value to check
      * @return True if argument is null or empty, else false.
      */
@@ -79,19 +115,36 @@ public class Checker {
         return (value == null || value.length == 0);
     }
 
-    //*************************************************************************
-    //* Array of Objects
-    //*************************************************************************
+
+    /**
+     * @param value The value to check
+     * @return True if argument is not null nor empty, else false.
+     */
+    public static boolean isNotEmpty(double[] value) {
+        return !Checker.isEmpty(value);
+    }
+
 
     /**
      * Check if an array of objects is null or empty
      *
-     * @param <T> The type of the array
+     * @param <T>   The type of the array
      * @param value The value to check
-     * @return True if argument is empty or length 0, else false. 
+     * @return True if argument is empty or length 0, else false.
      */
     public static <T> boolean isEmpty(T[] value) {
         return (value == null || value.length == 0);
     }
 
+
+    /**
+     * Check if an array of objects is null or empty
+     *
+     * @param <T>   The type of the array
+     * @param value The value to check
+     * @return True if argument is not empty nor length 0, else false.
+     */
+    public static <T> boolean isNotEmpty(T[] value) {
+        return !Checker.isEmpty(value);
+    }
 }
