c176059b250098bb173a2cc449e212e7008651b3
Schinzel <henrik@schinzel.se>
2017-04-29 10:21:59 +0200

    Skeleton code

--- src/main/java/io/schinzel/basicutils/substringer/SubStringer.java
+++ src/main/java/io/schinzel/basicutils/substringer/SubStringer.java
@@ -1,52 +1,101 @@
 package io.schinzel.basicutils.substringer;
 
+import io.schinzel.basicutils.Checker;
 import io.schinzel.basicutils.Thrower;
-import lombok.Setter;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.experimental.Accessors;
 
 /**
- * The purpose of this class is to extract substring from a string between
- * a start and end string.
+ * The purpose of this class is to extract substring from a string between a start and end string.
  *
  * @author Schinzel
  */
-@Accessors(prefix = "m", chain = true, fluent = true)
+@Accessors(prefix = "m")
 public class SubStringer {
     /** Flag that indicates that no value has been set. */
     private static final String NO_VAL_SET = "qEvLh6L7HJ6uAkoJB7kT";
-    /** The string from which to extract a substring. */
-    private final String mStr;
-    /** The start delimiter string. */
-    @Setter
-    private String mStartDelimiter = NO_VAL_SET;
-    /** Which occurrence of the start delimiter to look for */
-    @Setter
-    private int mStartOccurrence = 1;
-    /** The end delimiter string. */
-    @Setter
-    private String mEndDelimiter = NO_VAL_SET;
-    /** Which occurrence of the end delimiter to look for */
-    @Setter
-    private int mEndOccurrence = 1;
+    /** Extracted substring */
+    @Getter private final String mSubString;
+
+
+    @Builder
+    private SubStringer(String string, String startDelimiter, int startOccurrence, String endDelimiter, int endOccurrence) {
+        Thrower.throwIfVarNull(string, "string");
+        //If start delimiter was not set (or was set to null or empty string)
+        if (Checker.isEmpty(startDelimiter)) {
+            startDelimiter = NO_VAL_SET;
+        }
+        //If start delimiter was not set (or was set to null or empty string)
+        if (Checker.isEmpty(endDelimiter)) {
+            endDelimiter = NO_VAL_SET;
+        }
+        int startPos = getStartPos(string, startDelimiter, startOccurrence);
+        int endPos = getEndPos(string, endDelimiter, endOccurrence, startPos);
+        mSubString = string.substring(startPos, endPos);
+
+    }
+    /**
+     * To Do:
+     * - Byta namn till SubString?
+     * - Hur används contains-metoden som nog måste bort.
+     * - Tester
+     * - Få resten av projekt att funkar
+     * - Sample
+     * - Read me: Release notes
+     * - Read me: sample
+     */
 
 
     /**
-     * Private constructor so that is not used directly.
-     *
-     * @param str The string from which to extract a substring.
+     * @return A new sub-stringer builder for extracting a string from an extracted string.
      */
-    private SubStringer(String str) {
-        Thrower.throwIfVarNull(str, "str");
-        mStr = str;
+    public SubStringer.SubStringerBuilder getSubStringer() {
+        return SubStringer.builder().string(this.getSubString());
     }
 
 
     /**
-     * @param str The string from which to extract a substring.
-     * @return An instance.
+     * @param s The string to look for
+     * @return True if constructor set string contains argument string.
      */
-    public static SubStringer create(String str) {
-        return new SubStringer(str);
+    public boolean contains(String s) {
+        return this.getSubString().contains(s);
+    }
+
+
+    static int getStartPos(String string, String startDelimiter, int startOccurrence) {
+        //If no start delimiter has been set
+        if (!valueSet(startDelimiter)) {
+            //Let the start pos be start of string.
+            return 0;
+        }
+        SubstringIndexFinder startIndex = SubstringIndexFinder.builder()
+                .string(string)
+                .subString(startDelimiter)
+                .occurrence(startOccurrence)
+                .build();
+        return startIndex.isSubstringFound()
+                ? startIndex.getSubstringPosition() + startDelimiter.length()
+                : string.length();
+    }
+
+
+    static int getEndPos(String string, String endDelimiter, int endOccurrence, int startPos) {
+        //If not end delimiter has been set
+        if (!valueSet(endDelimiter)) {
+            //Let the end pos be end of string
+            return string.length();
+        }
+        SubstringIndexFinder endIndex = SubstringIndexFinder.builder()
+                .string(string)
+                .startPos(startPos)
+                .subString(endDelimiter)
+                .occurrence(endOccurrence)
+                .build();
+        return endIndex.isSubstringFound()
+                ? endIndex.getSubstringPosition()
+                : string.length();
     }
 
 
@@ -59,70 +108,4 @@ public class SubStringer {
     }
 
 
-    /**
-     * @return A new substring which takes the generated substring as input.
-     */
-    public SubStringer getSubStringer() {
-        return new SubStringer(this.toString());
-    }
-
-
-    /**
-     * @param s The string to look for,
-     * @return True if constructor set string contains argument string.
-     */
-    public boolean contains(String s) {
-        return mStr.contains(s);
-    }
-
-
-    int getStartPos() {
-        //If no start delimiter has been set
-        if (!valueSet(mStartDelimiter)) {
-            //Let the start pos be start of string.
-            return 0;
-        }
-        SubstringIndexFinder startIndex = SubstringIndexFinder.builder()
-                .string(mStr)
-                .subString(mStartDelimiter)
-                .occurrence(mStartOccurrence)
-                .build();
-        return startIndex.isSubstringFound()
-                ? startIndex.getSubstringPosition() + mStartDelimiter.length()
-                : mStr.length();
-    }
-
-
-    int getEndPos(int startPos) {
-        //If not end delimiter has been set
-        if (!valueSet(mEndDelimiter)) {
-            //Let the end pos be end of string
-            return mStr.length();
-        }
-        SubstringIndexFinder endIndex = SubstringIndexFinder.builder()
-                .string(mStr)
-                .startPos(startPos)
-                .subString(mEndDelimiter)
-                .occurrence(mEndOccurrence)
-                .build();
-        return endIndex.isSubstringFound()
-                ? endIndex.getSubstringPosition()
-                : mStr.length();
-    }
-
-
-    /**
-     * @return The substring between the start and end delimiter, exclusive the
-     * delimiters. If no start delimiter has been set, start of string is
-     * assumed. If no end delimiter has been set, end of string is assumed.
-     */
-    @Override
-    public String toString() {
-        //Throw exception if either start or end delimiter has been set to empty value.
-        Thrower.throwIfVarEmpty(mStartDelimiter, "StartDelimiter");
-        Thrower.throwIfVarEmpty(mEndDelimiter, "EndDelimiter");
-        int startPos = this.getStartPos();
-        int endPos = this.getEndPos(startPos);
-        return mStr.substring(startPos, endPos);
-    }
 }
