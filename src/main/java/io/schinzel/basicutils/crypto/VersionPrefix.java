d802378ca0ce95754290a1faf4745a7e9a32dc82
Schinzel <henrik@schinzel.se>
2017-04-30 15:04:20 +0200

    Version prefix class added

---
+++ src/main/java/io/schinzel/basicutils/crypto/VersionPrefix.java
@@ -0,0 +1,43 @@
+package io.schinzel.basicutils.crypto;
+
+import io.schinzel.basicutils.Thrower;
+
+/**
+ * The purpose of this class is to handle versioning of strings. The version is in a prefix.
+ * This is useful for for example encryption, where you want to change keys or algorithm over
+ * time for new data but still support decrypting the older encrypted data.
+ * <p>
+ * Created by schinzel on 2017-04-30.
+ */
+public class VersionPrefix {
+    private final String mPrefix;
+
+
+    public static VersionPrefix create(int version) {
+        return new VersionPrefix(version);
+    }
+
+
+    VersionPrefix(int version) {
+        Thrower.throwIfVarOutsideRange(version, "version", 1, 1000000);
+        mPrefix = "v" + String.valueOf(version) + "_";
+    }
+
+
+    public String addPrefix(String string) {
+        return mPrefix + string;
+    }
+
+
+    public String removePrefix(String string) {
+        if (!this.hasThisPrefix(string)) {
+            throw new RuntimeException("Cannot remove prefix '" + mPrefix + "' from string '" + string + "' as it has no such prefix.");
+        }
+        return string.substring(mPrefix.length(), string.length());
+    }
+
+
+    public boolean hasThisPrefix(String string) {
+        return string.startsWith(mPrefix);
+    }
+}
