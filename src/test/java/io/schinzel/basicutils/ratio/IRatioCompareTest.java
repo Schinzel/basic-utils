09715ec2b7ab3972420a3cb350499803801888ec
Schinzel <henrik@schinzel.io>
2017-07-23 08:20:04 +0200

    Isolated test class

--- src/test/java/io/schinzel/basicutils/ratio/IRatioCompareTest.java
+++ src/test/java/io/schinzel/basicutils/ratio/IRatioCompareTest.java
@@ -13,33 +13,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
 
-public class IRatioCompareTest extends AbstractRatio<IRatioCompareTest> implements IRatioCompare<IRatioCompareTest> {
+public class IRatioCompareTest {
 
-    public IRatioCompareTest() {
-        this(1, 1);
-    }
-
-
-    IRatioCompareTest(int num, int den){
-        super(BigInteger.valueOf(num), BigInteger.valueOf(den));
-    }
+    private class RatioCompareTest extends AbstractRatio<RatioCompareTest> implements IRatioCompare<RatioCompareTest> {
+        RatioCompareTest(int num, int den) {
+            super(BigInteger.valueOf(num), BigInteger.valueOf(den));
+        }
 
 
-    @Override
-    public IRatioCompareTest newInstance(BigInteger numerator, BigInteger denominator) {
-        throw new RuntimeException("Not implemented!");
+        @Override
+        public RatioCompareTest newInstance(BigInteger numerator, BigInteger denominator) {
+            throw new RuntimeException("Not implemented!");
+        }
+
     }
 
 
     @Test
     public void testCompare() {
-        IRatioCompareTest r1 = new IRatioCompareTest(1, 3);
-        IRatioCompareTest r2 = new IRatioCompareTest(1, 3);
+        RatioCompareTest r1 = new RatioCompareTest(1, 3);
+        RatioCompareTest r2 = new RatioCompareTest(1, 3);
         int result = r1.compareTo(r2);
         Assert.assertEquals(0, result);
         //
-        r1 = new IRatioCompareTest(1, 3);
-        r2 = new IRatioCompareTest(2, 3);
+        r1 = new RatioCompareTest(1, 3);
+        r2 = new RatioCompareTest(2, 3);
         result = r1.compareTo(r2);
         assertThat(result).isLessThan(0);
         //
@@ -50,10 +48,10 @@ public class IRatioCompareTest extends AbstractRatio<IRatioCompareTest> implemen
 
     @Test
     public void testGreaterThan() {
-        IRatioCompareTest r1 = new IRatioCompareTest(1, 4);
-        IRatioCompareTest r2 = new IRatioCompareTest(2, 4);
-        IRatioCompareTest r3 = new IRatioCompareTest(3, 4);
-        IRatioCompareTest r4 = new IRatioCompareTest(4, 4);
+        RatioCompareTest r1 = new RatioCompareTest(1, 4);
+        RatioCompareTest r2 = new RatioCompareTest(2, 4);
+        RatioCompareTest r3 = new RatioCompareTest(3, 4);
+        RatioCompareTest r4 = new RatioCompareTest(4, 4);
         assertTrue(r4.greaterThan(r3));
         assertTrue(r3.greaterThan(r2));
         assertTrue(r2.greaterThan(r1));
@@ -62,9 +60,9 @@ public class IRatioCompareTest extends AbstractRatio<IRatioCompareTest> implemen
 
     @Test
     public void testGreaterThan2() {
-        IRatioCompareTest r1 = new IRatioCompareTest(1, 4);
-        IRatioCompareTest r2 = new IRatioCompareTest(1, 3);
-        IRatioCompareTest r3 = new IRatioCompareTest(1, 2);
+        RatioCompareTest r1 = new RatioCompareTest(1, 4);
+        RatioCompareTest r2 = new RatioCompareTest(1, 3);
+        RatioCompareTest r3 = new RatioCompareTest(1, 2);
         assertTrue(r3.greaterThan(r2));
         assertTrue(r2.greaterThan(r1));
     }
@@ -72,10 +70,10 @@ public class IRatioCompareTest extends AbstractRatio<IRatioCompareTest> implemen
 
     @Test
     public void testLessThan() {
-        IRatioCompareTest r1 = new IRatioCompareTest(4, 4);
-        IRatioCompareTest r2 = new IRatioCompareTest(3, 4);
-        IRatioCompareTest r3 = new IRatioCompareTest(2, 4);
-        IRatioCompareTest r4 = new IRatioCompareTest(1, 4);
+        RatioCompareTest r1 = new RatioCompareTest(4, 4);
+        RatioCompareTest r2 = new RatioCompareTest(3, 4);
+        RatioCompareTest r3 = new RatioCompareTest(2, 4);
+        RatioCompareTest r4 = new RatioCompareTest(1, 4);
         assertTrue(r4.lessThan(r3));
         assertTrue(r3.lessThan(r2));
         assertTrue(r2.lessThan(r1));
@@ -84,9 +82,9 @@ public class IRatioCompareTest extends AbstractRatio<IRatioCompareTest> implemen
 
     @Test
     public void testLessThan2() {
-        IRatioCompareTest r1 = new IRatioCompareTest(1, 2);
-        IRatioCompareTest r2 = new IRatioCompareTest(1, 3);
-        IRatioCompareTest r3 = new IRatioCompareTest(1, 4);
+        RatioCompareTest r1 = new RatioCompareTest(1, 2);
+        RatioCompareTest r2 = new RatioCompareTest(1, 3);
+        RatioCompareTest r3 = new RatioCompareTest(1, 4);
         assertTrue(r3.lessThan(r2));
         assertTrue(r2.lessThan(r1));
     }
@@ -94,10 +92,10 @@ public class IRatioCompareTest extends AbstractRatio<IRatioCompareTest> implemen
 
     @Test
     public void testSort() {
-        IRatioCompareTest r1 = new IRatioCompareTest(3, 4);
-        IRatioCompareTest r2 = new IRatioCompareTest(1, 4);
-        IRatioCompareTest r3 = new IRatioCompareTest(2, 4);
-        List<IRatioCompareTest> list = Arrays.asList(r1, r2, r3);
+        RatioCompareTest r1 = new RatioCompareTest(3, 4);
+        RatioCompareTest r2 = new RatioCompareTest(1, 4);
+        RatioCompareTest r3 = new RatioCompareTest(2, 4);
+        List<RatioCompareTest> list = Arrays.asList(r1, r2, r3);
         Collections.sort(list);
         assertTrue(list.get(0).lessThan(list.get(1)));
         assertTrue(list.get(1).lessThan(list.get(2)));
@@ -106,12 +104,12 @@ public class IRatioCompareTest extends AbstractRatio<IRatioCompareTest> implemen
 
     @Test
     public void testEquals() {
-        IRatioCompareTest r1 = new IRatioCompareTest(2, 4);
-        IRatioCompareTest r2 = new IRatioCompareTest(1, 2);
+        RatioCompareTest r1 = new RatioCompareTest(2, 4);
+        RatioCompareTest r2 = new RatioCompareTest(1, 2);
         assertTrue(r1.equals(r2));
         assertTrue(r2.equals(r1));
-        r1 = new IRatioCompareTest(1, 3);
-        r2 = new IRatioCompareTest(1, 2);
+        r1 = new RatioCompareTest(1, 3);
+        r2 = new RatioCompareTest(1, 2);
         assertFalse(r1.equals(r2));
         assertFalse(r2.equals(r1));
     }
