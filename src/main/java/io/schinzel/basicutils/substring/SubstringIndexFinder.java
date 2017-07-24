3089928e8b04c7bef64ffcb7122814a49ac059e4
Schinzel <henrik@schinzel.io>
2017-07-24 14:35:02 +0200

    Doc and sample

--- src/main/java/io/schinzel/basicutils/substring/SubstringIndexFinder.java
+++ src/main/java/io/schinzel/basicutils/substring/SubstringIndexFinder.java
@@ -25,17 +25,30 @@ class SubstringIndexFinder {
     }
 
 
+    /**
+     *
+     * @param string The string in which to look for the sub string
+     * @param subString The sub string to look after
+     * @param occurrence The occurrence of the sub string
+     * @param startPos From which index to look for the sub string
+     * @return The index of the argument sub string in the argument string. -1 if the substring was not found.
+     */
     static int getPos(String string, String subString, int occurrence, int startPos) {
         Thrower.throwIfVarTooSmall(occurrence, "occurrence", 1);
         Thrower.throwIfVarTooSmall(occurrence, "startPos", 0);
         int pos = string.indexOf(subString, startPos);
+        //If substring was not found
         if (pos == -1) {
+            //Return not found flag
             return -1;
-        }
-        if (occurrence == 1) {
-            return pos;
-        } else {
-            return getPos(string, subString, --occurrence, pos + subString.length());
+        }// else, i.e. the sub string was found
+        else {
+            //If this was the look after substring occurrence
+            return (occurrence == 1)
+                    //Return this position
+                    ? pos
+                    //Do recursive request for the next occurrence of the substring
+                    : getPos(string, subString, --occurrence, pos + subString.length());
         }
     }
 }
---
+++ src/main/java/io/schinzel/samples/SubStringSample.java
@@ -0,0 +1,44 @@
+package io.schinzel.samples;
+
+import io.schinzel.basicutils.substring.SubString;
+
+/**
+ * The purpose of this file is to show SubStringer sample code.
+ * <p>
+ * Created by schinzel on 2017-02-27.
+ */
+class SubStringSample {
+    public static void main(String[] args) {
+        String input = "http://www.example.com/index.html?key1=val1&key2=val2";
+        //Get everything after question mark, i.e. "key1=val1&key2=val2"
+        SubString.create(input)
+                .startDelimiter("?")
+                .getStr()
+                .plnWithPrefix("Query string: ");
+        //Get everything before question mark, i.e. "http://www.example.com/index.html"
+        SubString.create(input)
+                .endDelimiter("?")
+                .getStr()
+                .plnWithPrefix("URL: ");
+        //Get host, i.e. "www.example.com"
+        SubString.create(input)
+                .startDelimiter("http://")
+                .endDelimiter("/index")
+                .getStr()
+                .plnWithPrefix("Host: ");
+        //First get "www.example.com/index.html", then get everything after the slash, i.e. "index.html"
+        SubString.create(input)
+                .startDelimiter("//")
+                .endDelimiter("?")
+                .newSubString()
+                .startDelimiter("/")
+                .getStr()
+                .plnWithPrefix("Page: ");
+        //Get everything after the second equals sign
+        SubString.create(input)
+                .startDelimiter("=")
+                .startOccurrence(2)
+                .getStr()
+                .plnWithPrefix("Second value: ");
+    }
+}
