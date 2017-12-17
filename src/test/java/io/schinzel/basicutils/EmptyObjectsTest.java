ace1aa54e547deabc055c2ad260013ae588bc9fe
Henrik Schinzel <henrik@schinzel.io>
2017-12-17 19:27:04 +0100

    Refactored tests

--- src/test/java/io/schinzel/basicutils/EmptyObjectsTest.java
+++ src/test/java/io/schinzel/basicutils/EmptyObjectsTest.java
@@ -10,34 +10,75 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class EmptyObjectsTest extends EmptyObjects {
 
+
     @Test
-    public void testSomeMethod() {
-        assertThat(EmptyObjects.EMPTY_BYTE_ARRAY)
-                .isNotNull()
-                .isEmpty();
-        assertThat(EmptyObjects.EMPTY_INT_ARRAY)
-                .isNotNull()
-                .isEmpty();
-        assertThat(EmptyObjects.EMPTY_STRING_ARRAY)
-                .isNotNull()
-                .isEmpty();
-        assertThat(EmptyObjects.EMPTY_OBJECT_ARRAY)
-                .isNotNull()
-                .isEmpty();
-        assertThat(EmptyObjects.EMPTY_DOUBLE_ARRAY)
-                .isNotNull()
-                .isEmpty();
-        assertThat(EmptyObjects.EMPTY_FLOAT_ARRAY)
-                .isNotNull()
-                .isEmpty();
-        assertThat(EmptyObjects.EMPTY_LONG_ARRAY)
-                .isNotNull()
-                .isEmpty();
+    public void emptyString_EmptyString() {
+        assertThat(EmptyObjects.EMPTY_STRING)
+                .isEqualTo("");
+    }
+
+
+    @Test
+    public void emptyBooleanArray_EmptyNonNull() {
         assertThat(EmptyObjects.EMPTY_BOOLEAN_ARRAY)
                 .isNotNull()
                 .isEmpty();
-        assertThat(EmptyObjects.EMPTY_STRING)
-                .isEqualTo("");
+    }
+
+
+    @Test
+    public void emptyLongArray_EmptyNonNull() {
+        assertThat(EmptyObjects.EMPTY_LONG_ARRAY)
+                .isNotNull()
+                .isEmpty();
+    }
+
+
+    @Test
+    public void emptyFloatArray_EmptyNonNull() {
+        assertThat(EmptyObjects.EMPTY_FLOAT_ARRAY)
+                .isNotNull()
+                .isEmpty();
+    }
+
+
+    @Test
+    public void emptyDoubleArray_EmptyNonNull() {
+        assertThat(EmptyObjects.EMPTY_DOUBLE_ARRAY)
+                .isNotNull()
+                .isEmpty();
+    }
+
+
+    @Test
+    public void emptyObjectArray_EmptyNonNull() {
+        assertThat(EmptyObjects.EMPTY_OBJECT_ARRAY)
+                .isNotNull()
+                .isEmpty();
+    }
+
+
+    @Test
+    public void emptyStringArray_EmptyNonNull() {
+        assertThat(EmptyObjects.EMPTY_STRING_ARRAY)
+                .isNotNull()
+                .isEmpty();
+    }
+
+
+    @Test
+    public void emptyIntArray_EmptyNonNull() {
+        assertThat(EmptyObjects.EMPTY_INT_ARRAY)
+                .isNotNull()
+                .isEmpty();
+    }
+
+
+    @Test
+    public void emptyByteArray_EmptyNonNull() {
+        assertThat(EmptyObjects.EMPTY_BYTE_ARRAY)
+                .isNotNull()
+                .isEmpty();
     }
 
 
