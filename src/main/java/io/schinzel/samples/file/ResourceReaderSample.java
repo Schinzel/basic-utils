35779ba7f01e25cffddd605d8df94f596a6aa536
Henrik Schinzel <henrik@schinzel.io>
2018-06-20 13:52:08 +0200

    Old design ResourceReader removed

--- src/main/java/io/schinzel/samples/file/ResourceReaderSample.java
+++ src/main/java/io/schinzel/samples/file/ResourceReaderSample.java
@@ -1,6 +1,6 @@
 package io.schinzel.samples.file;
 
-import io.schinzel.basicutils.file.ResourceReader;
+import io.schinzel.basicutils.file.ResourceReader2;
 
 /**
  * Purpose of this class is to provide sample usage of ResourceReader
@@ -10,7 +10,9 @@ import io.schinzel.basicutils.file.ResourceReader;
 public class ResourceReaderSample {
 
     public static void main(String[] args) {
-        String read = ResourceReader.read("io/schinzel/samples/sample_resource.txt");
+        String read = ResourceReader2
+                .read("io/schinzel/samples/sample_resource.txt")
+                .asString();
         System.out.println(read);
     }
 }
--- src/test/java/io/schinzel/basicutils/file/ResourceReaderTest.java
+++ src/test/java/io/schinzel/basicutils/file/ResourceReaderTest.java
@@ -1,51 +1,39 @@
 package io.schinzel.basicutils.file;
 
-import io.schinzel.basicutils.UTF8;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 
 /**
  * Created by Schinzel on 2018-01-13
  */
 public class ResourceReaderTest {
-    private final String mFileName = "resource_test_file.txt";
-
+   
 
     @Test
     public void read_ExistingFile_FileContent() {
-        String stringRead = ResourceReader.read(mFileName);
+        String stringRead = ResourceReader2
+                .read("resource_test_file.txt")
+                .asString();
         assertThat(stringRead).isEqualTo("File content");
     }
 
 
     @Test
     public void read_ExistingFileInSubDir_FileContent() {
-        String stringRead = ResourceReader.read("dir/another_resource_test_file.txt");
+        String stringRead = ResourceReader2
+                .read("dir/another_resource_test_file.txt")
+                .asString();
         assertThat(stringRead).isEqualTo("File content 2");
     }
 
 
     @Test
-    public void readAsByteArray_ExistingFile_FileContent() {
-        byte[] bytesRead = ResourceReader.readAsByteArray(mFileName);
-        String stringRead = UTF8.getString(bytesRead);
-        assertThat(stringRead).isEqualTo("File content");
-    }
-
-
-    @Test
-    public void readAsStr_ExistingFile_FileContent() {
-        String stringRead = ResourceReader.readAsStr(mFileName).getString();
-        assertThat(stringRead).isEqualTo("File content");
-    }
-
-
-    @Test
-    public void readAsByteArrayInternal_ThrowException_Exception() {
+    public void read_ThrowException_Exception() {
         assertThatExceptionOfType(RuntimeException.class).isThrownBy(() ->
-                ResourceReader.readAsByteArrayInternal(mFileName, true)
+                ResourceReader2.read("resource_test_file.txt", true)
         ).withMessageStartingWith("Error when reading resource file");
     }
 }
